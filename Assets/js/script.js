0d6b0cfd930cef971d8de6114d0519b325f0cd3c
Elijah Brown <elijahab716@gmail.com>
2024-05-22 17:38:55 -0500

    properly added theme switch functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,17 +1,19 @@
-const themeButton = document.getElementById('toggle-theme')
-const currentTheme = localStorage.getItem('theme') || 'light';
-document.body.setAttribute('data-theme', currentTheme)
+//kept getting error: TypeError: Cannot read properties of null (reading 'setAttribute') on lines 13 and 16
+//looked online to find that the error may have stemmed from the js file running before the document body was fully loaded
+//'DOMContentLoaded' watches for that
+document.addEventListener('DOMContentLoaded', function() {
+    const themeButton = document.querySelector('#toggle-theme');
+    const form = document.querySelector('#main');
+    let theme = 'light';
 
-themeButton.addEventListener('click', function() {
-    console.log('hit')
-    let theme;
-    if (currentTheme === 'light') {
-        theme = 'dark';
-        document.body.setAttribute('class', 'dark')
-    } else {
-        theme = 'light';
-        document.body.setAttribute('class', 'light')
-    }
-    document.body.setAttribute('data-theme', theme)
-    localStorage.setItem('theme', theme)
-})
+    themeButton.addEventListener('click', function() {
+        console.log('hit');
+        if (theme === 'light') {
+            theme = 'dark';
+            form.setAttribute('class', 'dark');
+        } else {
+            theme = 'light';
+            form.setAttribute('class', 'light');
+        }
+    });
+});
